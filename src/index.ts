192c9d5c7a34be009be083a599d0f37a5fd90fe3
Hoang Nam <nam20019999@gmail.com>
2022-05-22 19:29:43 +0700

    Initial files

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.text({ type: "text/html" }));
 app.use(express.static(path.join(__dirname, "../public")));
 
-app.get("/", async (req, res) => {
+app.get("/", (req, res) => {
   res.send("Welcome to Files Provider !");
 });
 
